8c32ed35ebe43400a93aeb648d665c08efc472e2
Max Yu <130877963+hkpeaks@users.noreply.github.com>
2023-10-12 22:32:25 +0800

    Update duckdb-parquet.py

--- cf-002 InnerJoin-GroupBy/Large-Filter-Range/duckdb-parquet.py
+++ cf-002 InnerJoin-GroupBy/Large-Filter-Range/duckdb-parquet.py
@@ -8,13 +8,13 @@ start_time = time.time()
 conn = duckdb.connect()
 
 # Execute SQL queries using duckdb.sql()
-duckdb.sql("CREATE TABLE master AS SELECT * FROM parquet_scan('Inbox/Master.parquet')")
-duckdb.sql("CREATE VIEW filter_master AS SELECT * FROM master WHERE Style >= 'B' AND Style <= 'F'")
+conn.execute("CREATE TABLE master AS SELECT * FROM parquet_scan('Inbox/Master.parquet')")
+conn.execute("CREATE VIEW filter_master AS SELECT * FROM master WHERE Style >= 'B' AND Style <= 'F'")
 
 # Use Python's f-string for string formatting
-duckdb.sql(f"CREATE TABLE fact_table AS SELECT * FROM parquet_scan('Inbox/{sys.argv[1]}')")
+conn.execute(f"CREATE TABLE fact_table AS SELECT * FROM parquet_scan('Inbox/{sys.argv[1]}')")
 
-duckdb.sql("""
+conn.execute("""
     CREATE VIEW detail_result AS
     SELECT fact_table.*, Quantity * Unit_Price AS Amount
     FROM fact_table
@@ -22,7 +22,7 @@ duckdb.sql("""
     WHERE Shop >= 'S21' AND Shop <= 'S99' AND Amount > 1000
 """)
 
-duckdb.sql("""
+conn.execute("""
     CREATE VIEW summary_result AS
     SELECT Shop, Product, COUNT(*) AS Count, SUM(Quantity) AS Sum_Quantity, SUM(Amount) AS Sum_Amount
     FROM detail_result
@@ -32,24 +32,20 @@ duckdb.sql("""
 # Save the detail_result and summary_result to parquet files directly using duckdb.sql() instead of fetchdf()
 detail_filename = f"Outbox/DuckDB_Detail_Result_{sys.argv[1].replace('.parquet', '')}.parquet"
 summary_filename = f"Outbox/DuckDB_Summary_Result_{sys.argv[1].replace('.parquet', '')}.parquet"
-duckdb.sql(f"COPY (SELECT * FROM detail_result) TO '{detail_filename}' (FORMAT PARQUET)")
-duckdb.sql(f"COPY (SELECT * FROM summary_result) TO '{summary_filename}' (FORMAT PARQUET)")
+conn.execute(f"COPY (SELECT * FROM detail_result) TO '{detail_filename}' (FORMAT PARQUET)")
+conn.execute(f"COPY (SELECT * FROM summary_result) TO '{summary_filename}' (FORMAT PARQUET)")
+
+# Fetch the first 10 rows from the detail_result and summary_result views and print them to the CLI in a list of tuples format
+detail_result = conn.execute("SELECT * FROM detail_result LIMIT 10").fetchall()
+summary_result = conn.execute("SELECT * FROM summary_result LIMIT 10").fetchall()
+
+print("\nDetail Result (First 10 Rows):")
+for row in detail_result:
+    print(row)
+
+print("\nSummary Result (First 10 Rows):")
+for row in summary_result:
+    print(row)
 
 end_time = time.time()
-print("DuckDB Parquet Duration (In Second): {}".format(round(end_time-start_time,3)))
-
-
-
-"""
-D:\Benchmarking>python duckdb-parquet.py 1M_Fact.parquet
-DuckDB Parquet Duration (In Second): 0.173
-
-D:\Benchmarking>python duckdb-parquet.py 10M_Fact.parquet
-DuckDB Parquet Duration (In Second): 0.8
-
-D:\Benchmarking>python duckdb-parquet.py 100M_Fact.parquet
-DuckDB Parquet Duration (In Second): 9.035
-
-D:\Benchmarking>python duckdb-parquet.py 1000M_Fact.parquet
-DuckDB Parquet Duration (In Second): 573.928  """
-
+print("\nDuckDB Parquet Duration (In Second): {}".format(round(end_time-start_time,3)))
